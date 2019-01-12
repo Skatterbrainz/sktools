462873e9928af3e129a9e7feb0b414004bced49d
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-01-11 21:58:23 -0500

    Add files via upload

--- webui/reports/Devices - Counts by AD Site Name.sql
+++ webui/reports/Devices - Counts by AD Site Name.sql
@@ -1,9 +1,11 @@
 ﻿SELECT DISTINCT
-	COALESCE(AD_Site_Name0, 'Default') AS ADSiteName,
+	case
+		when (AD_Site_Name0 IS NULL) then 'Default'
+		else AD_Site_Name0 end as ADSiteName,
 	COUNT(*) AS Computers
 FROM
 	dbo.v_R_System
 GROUP BY
 	AD_SITE_Name0
 ORDER BY
-	ADSite
+	ADSiteName
