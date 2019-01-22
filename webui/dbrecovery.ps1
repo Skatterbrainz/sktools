7372bbcbd20c4e3175b28767b049711f78a1ba8c
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-01-22 01:09:54 -0500

    Add files via upload

--- webui/dbrecovery.ps1
+++ webui/dbrecovery.ps1
@@ -8,6 +8,7 @@ $xxx         = ""
 
 try {
     $content += (Get-DbaDbRecoveryModel -SqlInstance $SkCmDbHost -ErrorAction Continue | 
+		Select-Object Name,ComputerName,InstanceName,RecoveryModel,SizeMB,Compatibility,LastFullBackup,LastDiffBackup,LastLogBackup | 
         ConvertTo-Html -Fragment) -replace '<table>', '<table id=table1>'
 
 }
