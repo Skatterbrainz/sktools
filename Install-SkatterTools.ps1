dad50e2a56e4a92f7d1d4b4388a625f6e33b72a8
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-02-01 23:22:23 -0500

    Add files via upload

--- Install-SkatterTools.ps1
+++ Install-SkatterTools.ps1
@@ -17,31 +17,31 @@
 #>
 
 function Install-SkatterTools {
-    [CmdletBinding()]
-    param()
-    Write-Host "setting default configuration" -ForegroundColor Cyan
-    try {
-        $cfgfile = "$($env:USERPROFILE)\Documents\skconfig.txt"
-        Write-Verbose "config file: $cfgfile"
-        if (Test-Path $cfgfile) {
-            Write-Warning "overwriting $cfgfile with default settings!"
-        }
-        $params = [ordered]@{
-            _Comment               = "SkatterTools configuration file. Lines with underscore prefix are comments."
-            _LastUpdated           = (Get-Date)
+	[CmdletBinding()]
+	param()
+	Write-Host "setting default configuration" -ForegroundColor Cyan
+	try {
+		$cfgfile = "$($env:USERPROFILE)\Documents\skconfig.txt"
+		Write-Verbose "config file: $cfgfile"
+		if (Test-Path $cfgfile) {
+			Write-Warning "overwriting $cfgfile with default settings!"
+		}
+		$params = [ordered]@{
+			_Comment               = "SkatterTools configuration file. Lines with underscore prefix are comments."
+			_LastUpdated           = (Get-Date)
 			_ModuleVersion         = (Get-Module sktools).Version -join '.'
-            _UpdatedBy             = $env:USERNAME
-            _LocalHost             = $env:COMPUTERNAME
+			_UpdatedBy             = $env:USERNAME
+			_LocalHost             = $env:COMPUTERNAME
 			_UserDomain            = $env:USERDOMAIN
-            SkAPPNAME              = "SkatterTools"
-            SkTheme                = "stdark.css"
-            SkADEnabled            = "TRUE"
-            SkADGroupManage        = "TRUE"
-            SkCMEnabled            = "TRUE"
-            SkCmDBHost             = "cm01.contoso.local"
-            SkCmSMSProvider        = "cm01.contoso.local"
-            SkCmSiteCode           = "P01"
-            SkCmCollectionManage   = "TRUE"
+			SkAPPNAME              = "SkatterTools"
+			SkTheme                = "stdark.css"
+			SkADEnabled            = "TRUE"
+			SkADGroupManage        = "TRUE"
+			SkCMEnabled            = "TRUE"
+			SkCmDBHost             = "cm01.contoso.local"
+			SkCmSMSProvider        = "cm01.contoso.local"
+			SkCmSiteCode           = "P01"
+			SkCmCollectionManage   = "TRUE"
 			SkDebug                = "FALSE"
 			SkTabSelectAdUsers     = "A"
 			SkTabSelectAdGroups    = "A"
@@ -54,12 +54,12 @@ function Install-SkatterTools {
 			SkCmCollectionCheck    = "TRUE"
 			SkUseDashboard         = "TRUE"
 			SkToolsPath            = Join-Path -Path $env:USERPROFILE -ChildPath "Documents"
-        }
-        Write-Verbose "writing to $cfgfile"
-        $params.Keys | %{ "$($_) = $($params.Item($_))" } | Out-File $cfgfile
-        Write-Host "configuration saved to: $cfgfile" -ForegroundColor Cyan
+		}
+		Write-Verbose "writing to $cfgfile"
+		$params.Keys | %{ "$($_) = $($params.Item($_))" } | Out-File $cfgfile
+		Write-Host "configuration saved to: $cfgfile" -ForegroundColor Cyan
 		Write-Host "use the SETTINGS option within the console to change defaults" -ForegroundColor Green
-        $result = "success"
+		$result = "success"
     }
     catch {
         $result = "error"
